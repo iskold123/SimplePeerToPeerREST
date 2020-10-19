1129fbf6a5c3075d634652d0452e5001c80a203f
Mads Olsen <mads36g6@edu.easj.dk>
2020-10-19 13:55:43 +0200

    Add project files.

---
+++ PeerToPeerServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PeerToPeerServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server worker = new Server();
+            worker.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PeerToPeerServer/Server.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PeerToPeerServer
+{
+    public class Server
+    {
+        public void Start()
+        {
+            TcpListener server = new TcpListener(IPAddress.Loopback, 4000);
+
+            server.Start();
+
+            while (true)
+            {
+                TcpClient socket = server.AcceptTcpClient();
+                Task.Run(() =>
+                {
+                    TcpClient tempSocket = socket;
+                    DoClient(tempSocket);
+                });
+            }
+        }
+
+        private void DoClient(TcpClient socket)
+        {
+            StreamReader sr = new StreamReader(socket.GetStream());
+            StreamWriter sw = new StreamWriter(socket.GetStream());
+
+            sw.Flush();
+
+            socket.Close();
+        }
+    }
+}
