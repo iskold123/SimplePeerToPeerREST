a484e7b63dbd728df21a72e719ea6a3ba9361fcf
muha1992 <58588384+muha1992@users.noreply.github.com>
2020-10-20 10:20:34 +0200

    modelklasse oprettet

---
+++ ModelLib/Model/FileEndPoint.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLib.Model
+{
+    public class FileEndPoint
+    {
+        private string _ipAddress;
+        private int _portNumber;
+        public string IpAddress
+        {
+            get => _ipAddress;
+            set => _ipAddress = value;
+        }
+
+        public int PortNumber
+        {
+            get => _portNumber;
+            set => _portNumber = value;
+        }
+
+        public FileEndPoint()
+        {
+
+        }
+        public FileEndPoint(string ipAddress, int portNumber)
+        {
+            _ipAddress = ipAddress;
+            _portNumber = portNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(IpAddress)}: {IpAddress}, {nameof(PortNumber)}: {PortNumber}";
+        }
+    }
+}
---
+++ SimplePeerToPeer/Controllers/PeerController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ModelLib.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SimplePeerToPeer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeerController : ControllerBase
+    {
+        private static Dictionary<string, FileEndPoint> _data = new Dictionary<string, FileEndPoint>
+        {
+
+
+        };
+        // GET: api/<PeerController>
+        [HttpGet]
+        [Route("api/powernap/{filename}")]
+        public IEnumerable<FileEndPoint> GetAll()
+        {
+            return _data(js);
+        }
+
+        // GET api/<PeerController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PeerController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PeerController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PeerController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
